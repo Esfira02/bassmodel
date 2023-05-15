2d6c4b7a7bbdd36d14fab735d488f503b8522ff5
Esfira02 <esfira.babajanyan2002@gmail.com>
2023-05-15 23:12:05 +0400

    Initial commit

---
+++ bassmodel/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for bassmodel."""
+
+__author__ = """Esfira Babajanyan"""
+__email__ = 'esfira.babajanyan2002@gmail.com'
+__version__ = '0.0.1'
---
+++ tests/test_bassmodel.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `bassmodel` package."""
+
+
+import unittest
+
+from bassmodel import bassmodel
+
+
+class TestBassmodel(unittest.TestCase):
+    """Tests for `bassmodel` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
